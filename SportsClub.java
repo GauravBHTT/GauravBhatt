64156e398d0b88bff943eb0a84060b29fd6d2bc0
GauravBHTT <113018324+GauravBHTT@users.noreply.github.com>
2022-09-08 21:38:21 -0700

    Add files via upload

---
+++ SportsClub.java
@@ -0,0 +1,26 @@
+/*  SportClub
+ * 
+    City
+    SportFeatures
+    Membership
+    Ragister_club
+    Services
+    Membership_plan
+    
+method
+    indoor game
+    outdoor game
+    life time membership
+    weekly------------------
+                    sorttuam
+    monthly----------------
+    tournament
+singal
+    player id,name,gender,
+static
+    playersname,dob,gender
+dynamic
+    mobilenumber,age,city,email
+multivalue
+    mobilenumber,email,address,playingroal
+*/
